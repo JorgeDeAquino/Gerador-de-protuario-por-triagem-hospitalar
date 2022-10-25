5cf26b509d02fea1fab0810f656c181df8bdbeb3
Jorge Alexandre <jorgeaamsantos@gmail.com>
2022-10-25 19:47:54 -0300

    add parametros basicos em JS

---
+++ main.js
@@ -0,0 +1,176 @@
+//frequencia respiratoria
+const fr = [
+    {
+      min: 20,
+      max: 24,
+      classificacao: "verde",
+    },
+    {
+      min: 20,
+      max: 34,
+      classificacao: "amarelo",
+    },
+    {
+      min: 35,
+      max: 80,
+      classificacao: "vermelho",
+    },
+  ];
+
+//frquencia cardiaca
+const fc = [
+    {
+      min: 60,
+      max: 119,
+      classificacao: "verde",
+    },
+    {
+      min: 120,
+      max: 139,
+      classificacao: "amarelo",
+    },
+    {
+      min: 0,
+      max: 59,
+      classificacao: "vermelho",
+    },
+    {
+      min: 140,
+      max: 250,
+      classificacao: "vermelho",
+    },
+  ];
+
+//pressão arterial sistolica
+const paSistolica = [
+    {
+        min: 120,
+        max: 149,
+        classificacao: "verde",
+      },
+      {
+        min: 150,
+        max: 169,
+        classificacao: "amarelo",
+      },
+      {
+        min: 0,
+        max: 119,
+        classificacao: "vermelho",
+      },
+      {
+        min: 170,
+        max: 250,
+        classificacao: "vermelho",
+      },
+  ];
+
+//pressão arterial diastolica
+const paDiastolica = [
+    {
+        min: 60,
+        max: 90,
+        classificacao: "verde",
+      },
+      {
+        min: 91,
+        max: 109,
+        classificacao: "amarelo",
+      },
+      {
+        min: 0,
+        max: 59,
+        classificacao: "vermelho",
+      },
+      {
+        min: 110,
+        max: 200,
+        classificacao: "vermelho",
+      },
+  ];
+
+  //temperatura axilar
+  const temperatura = [
+    {
+        min: 36,
+        max: 37.6,
+        classificacao: "verde",
+      },
+      {
+        min: 37.6,
+        max: 38.4,
+        classificacao: "amarelo",
+      },
+      {
+        min: 0,
+        max: 35.9,
+        classificacao: "vermelho",
+      },
+      {
+        min: 38.5,
+        max: 44,
+        classificacao: "vermelho",
+      },
+  ];
+
+//glicemia capilar
+const glicemia = [
+    {
+        min: 80,
+        max: 199,
+        classificacao: "verde",
+      },
+      {
+        min: 200,
+        max: 300,
+        classificacao: "amarelo",
+      },
+      {
+        min: 0,
+        max: 79,
+        classificacao: "vermelho",
+      },
+      {
+        min: 301,
+        max: 600,
+        classificacao: "vermelho",
+      },
+  ];
+
+//saturação de O2
+const saturacao = [
+    {
+        min: 96,
+        max: 100,
+        classificacao: "verde",
+      },
+      {
+        min: 90,
+        max: 95,
+        classificacao: "amarelo",
+      },
+      {
+        min: 0,
+        max: 89,
+        classificacao: "vermelho",
+      },
+  ];
+
+  //escala de dor
+const escalaDeDor = [
+    {
+        min: 0,
+        max: 4,
+        classificacao: "verde",
+      },
+      {
+        min: 5,
+        max: 7,
+        classificacao: "amarelo",
+      },
+      {
+        min: 8,
+        max: 10,
+        classificacao: "vermelho",
+      },
+  ];
